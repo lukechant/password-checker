28415a2393d72dc3266970493323a250ed86dd46
luke-chant <luke.chant@autotrader.co.uk>
2021-07-16 10:44:38 +0100

    add further test, refactored code further

--- app.js
+++ app.js
@@ -2,56 +2,34 @@ const passwordInput = document.getElementById("password-input");
 const progressBar = document.getElementById("progress-bar");
 let strength = document.getElementById("strength");
 
-
-
 export const passwordLengthCheck = function(inputLength) {
-    // console.log(passwordInput.value);
     let lengthValue = 0;
-
-    if (inputLength> 4 && inputLength <= 8) {
-        lengthValue = 20;
-    } else if(inputLength > 8 && inputLength <= 12) {
-        lengthValue = 40;
-    } else if(inputLength > 12 && inputLength <= 16) {
-        lengthValue = 60;
-    } else if(inputLength > 16 &&inputLength <=20) {
-        lengthValue = 80;
-    } else if(inputLength > 20) {
-        lengthValue = 100;
-    }
-    // console.log(inputLength);
-    // // console.log(lengthValue);
-    // progressBar.value = lengthValue;
-    return lengthValue;
-}
-
-
-export const strengthAssessor = function(inputLength) {
     let describeStrength = 'Pretty bad';
 
     if (inputLength> 4 && inputLength <= 8) {
+        lengthValue = 20;
         describeStrength = 'Weak';
     } else if(inputLength > 8 && inputLength <= 12) {
+        lengthValue = 40;
         describeStrength = 'OK';
     } else if(inputLength > 12 && inputLength <= 16) {
+        lengthValue = 60;
         describeStrength = 'Good';
     } else if(inputLength > 16 &&inputLength <=20) {
+        lengthValue = 80;
         describeStrength = 'Very good';
     } else if(inputLength > 20) {
+        lengthValue = 100;
         describeStrength = 'Excellent';
     }
-
-    strength.innerHTML = describeStrength;
+    return [lengthValue, describeStrength];
 }
 
 const updateDOM = function(newValue) {
-    progressBar.value = passwordLengthCheck(newValue);
-    
-    // strength.innerHTML =  strengthAssessor(newValue);
-    
+    progressBar.value = passwordLengthCheck(newValue)[0];
+    strength.innerHTML =  passwordLengthCheck(newValue)[1];
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     passwordInput.addEventListener('input', () => updateDOM(passwordInput.value.length));
-    // passwordInput.addEventListener('input', () => strengthAssessor(passwordInput.value.length));
 });
--- app.test.js
+++ app.test.js
@@ -5,6 +5,10 @@ import { strengthAssessor } from './app';
 describe('password checker', () => {
     it('should return lengthValue 40 when inputLength is 9', () => {
         const result = passwordLengthCheck(9);
-        expect(result).toEqual(40);
+        expect(result[0]).toEqual(40);
+    })
+    it('should return OK when inputLength is 9', () => {
+        const result = passwordLengthCheck(9);
+        expect(result[1]).toEqual('OK');
     })
 })
